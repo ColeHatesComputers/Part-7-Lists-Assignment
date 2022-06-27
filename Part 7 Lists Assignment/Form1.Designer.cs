6548056e2dff9b8cfc13d95a9a05745679edfebc
MyComputerBlewUp <99418457+ColeHatesComputers@users.noreply.github.com>
2022-06-27 09:48:35 -0400

    Add project files.

---
+++ Part 7 Lists Assignment/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Part_7_Lists_Assignment
+{
+    partial class frmLists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNewNumbers = new System.Windows.Forms.Button();
+            this.btnSortNumbers = new System.Windows.Forms.Button();
+            this.btnNewHeroes = new System.Windows.Forms.Button();
+            this.btnSortHeroes = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lstHeroes = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRemoveNumber = new System.Windows.Forms.Button();
+            this.btnRemoveAllNumbers = new System.Windows.Forms.Button();
+            this.txtAddHero = new System.Windows.Forms.TextBox();
+            this.txtRemoveHero = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddHero = new System.Windows.Forms.Button();
+            this.btnRemoveHero = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Wide Latin", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(366, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Heroes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Wide Latin", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numbers";
+            // 
+            // btnNewNumbers
+            // 
+            this.btnNewNumbers.BackColor = System.Drawing.Color.Tomato;
+            this.btnNewNumbers.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewNumbers.Location = new System.Drawing.Point(29, 67);
+            this.btnNewNumbers.Name = "btnNewNumbers";
+            this.btnNewNumbers.Size = new System.Drawing.Size(116, 23);
+            this.btnNewNumbers.TabIndex = 2;
+            this.btnNewNumbers.Text = "New List";
+            this.btnNewNumbers.UseVisualStyleBackColor = false;
+            this.btnNewNumbers.Click += new System.EventHandler(this.btnNewNumbers_Click);
+            // 
+            // btnSortNumbers
+            // 
+            this.btnSortNumbers.BackColor = System.Drawing.Color.Tomato;
+            this.btnSortNumbers.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnSortNumbers.Location = new System.Drawing.Point(151, 67);
+            this.btnSortNumbers.Name = "btnSortNumbers";
+            this.btnSortNumbers.Size = new System.Drawing.Size(123, 23);
+            this.btnSortNumbers.TabIndex = 3;
+            this.btnSortNumbers.Text = "Sort";
+            this.btnSortNumbers.UseVisualStyleBackColor = false;
+            this.btnSortNumbers.Click += new System.EventHandler(this.btnSortNumbers_Click);
+            // 
+            // btnNewHeroes
+            // 
+            this.btnNewHeroes.BackColor = System.Drawing.Color.Tomato;
+            this.btnNewHeroes.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnNewHeroes.Location = new System.Drawing.Point(332, 67);
+            this.btnNewHeroes.Name = "btnNewHeroes";
+            this.btnNewHeroes.Size = new System.Drawing.Size(118, 23);
+            this.btnNewHeroes.TabIndex = 4;
+            this.btnNewHeroes.Text = "New List";
+            this.btnNewHeroes.UseVisualStyleBackColor = false;
+            this.btnNewHeroes.Click += new System.EventHandler(this.btnNewHeroes_Click);
+            // 
+            // btnSortHeroes
+            // 
+            this.btnSortHeroes.BackColor = System.Drawing.Color.Tomato;
+            this.btnSortHeroes.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnSortHeroes.Location = new System.Drawing.Point(456, 67);
+            this.btnSortHeroes.Name = "btnSortHeroes";
+            this.btnSortHeroes.Size = new System.Drawing.Size(121, 23);
+            this.btnSortHeroes.TabIndex = 5;
+            this.btnSortHeroes.Text = "Sort";
+            this.btnSortHeroes.UseVisualStyleBackColor = false;
+            this.btnSortHeroes.Click += new System.EventHandler(this.btnSortHeroes_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(29, 96);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(245, 186);
+            this.lstNumbers.TabIndex = 6;
+            // 
+            // lstHeroes
+            // 
+            this.lstHeroes.FormattingEnabled = true;
+            this.lstHeroes.Location = new System.Drawing.Point(332, 97);
+            this.lstHeroes.Name = "lstHeroes";
+            this.lstHeroes.Size = new System.Drawing.Size(245, 186);
+            this.lstHeroes.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Wide Latin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 45);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Select a Number to Remove:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Wide Latin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(354, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 44);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Enter a Hero Name to Add:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRemoveNumber
+            // 
+            this.btnRemoveNumber.BackColor = System.Drawing.Color.Tomato;
+            this.btnRemoveNumber.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnRemoveNumber.Location = new System.Drawing.Point(29, 333);
+            this.btnRemoveNumber.Name = "btnRemoveNumber";
+            this.btnRemoveNumber.Size = new System.Drawing.Size(116, 23);
+            this.btnRemoveNumber.TabIndex = 11;
+            this.btnRemoveNumber.Text = "Remove";
+            this.btnRemoveNumber.UseVisualStyleBackColor = false;
+            this.btnRemoveNumber.Click += new System.EventHandler(this.btnRemoveNumber_Click);
+            // 
+            // btnRemoveAllNumbers
+            // 
+            this.btnRemoveAllNumbers.BackColor = System.Drawing.Color.Tomato;
+            this.btnRemoveAllNumbers.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnRemoveAllNumbers.Location = new System.Drawing.Point(151, 333);
+            this.btnRemoveAllNumbers.Name = "btnRemoveAllNumbers";
+            this.btnRemoveAllNumbers.Size = new System.Drawing.Size(123, 23);
+            this.btnRemoveAllNumbers.TabIndex = 12;
+            this.btnRemoveAllNumbers.Text = "Remove All";
+            this.btnRemoveAllNumbers.UseVisualStyleBackColor = false;
+            this.btnRemoveAllNumbers.Click += new System.EventHandler(this.btnRemoveAllNumbers_Click);
+            // 
+            // txtAddHero
+            // 
+            this.txtAddHero.Location = new System.Drawing.Point(358, 333);
+            this.txtAddHero.Name = "txtAddHero";
+            this.txtAddHero.Size = new System.Drawing.Size(196, 20);
+            this.txtAddHero.TabIndex = 13;
+            // 
+            // txtRemoveHero
+            // 
+            this.txtRemoveHero.Location = new System.Drawing.Point(358, 442);
+            this.txtRemoveHero.Name = "txtRemoveHero";
+            this.txtRemoveHero.Size = new System.Drawing.Size(196, 20);
+            this.txtRemoveHero.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Wide Latin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(354, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 62);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Enter a Hero Name to Remove:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAddHero
+            // 
+            this.btnAddHero.BackColor = System.Drawing.Color.Tomato;
+            this.btnAddHero.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnAddHero.Location = new System.Drawing.Point(424, 359);
+            this.btnAddHero.Name = "btnAddHero";
+            this.btnAddHero.Size = new System.Drawing.Size(75, 23);
+            this.btnAddHero.TabIndex = 16;
+            this.btnAddHero.Text = "Add";
+            this.btnAddHero.UseVisualStyleBackColor = false;
+            this.btnAddHero.Click += new System.EventHandler(this.btnAddHero_Click);
+            // 
+            // btnRemoveHero
+            // 
+            this.btnRemoveHero.BackColor = System.Drawing.Color.Tomato;
+            this.btnRemoveHero.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnRemoveHero.Location = new System.Drawing.Point(424, 468);
+            this.btnRemoveHero.Name = "btnRemoveHero";
+            this.btnRemoveHero.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveHero.TabIndex = 17;
+            this.btnRemoveHero.Text = "Remove";
+            this.btnRemoveHero.UseVisualStyleBackColor = false;
+            this.btnRemoveHero.Click += new System.EventHandler(this.btnRemoveHero_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Wide Latin", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(12, 377);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(336, 128);
+            this.lblStatus.TabIndex = 18;
+            this.lblStatus.Text = "Status:";
+            // 
+            // frmLists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(608, 514);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnRemoveHero);
+            this.Controls.Add(this.btnAddHero);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRemoveHero);
+            this.Controls.Add(this.txtAddHero);
+            this.Controls.Add(this.btnRemoveAllNumbers);
+            this.Controls.Add(this.btnRemoveNumber);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstHeroes);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnSortHeroes);
+            this.Controls.Add(this.btnNewHeroes);
+            this.Controls.Add(this.btnSortNumbers);
+            this.Controls.Add(this.btnNewNumbers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLists";
+            this.Text = "Part 7 Lists";
+            this.Load += new System.EventHandler(this.frmLists_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNewNumbers;
+        private System.Windows.Forms.Button btnSortNumbers;
+        private System.Windows.Forms.Button btnNewHeroes;
+        private System.Windows.Forms.Button btnSortHeroes;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.ListBox lstHeroes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRemoveNumber;
+        private System.Windows.Forms.Button btnRemoveAllNumbers;
+        private System.Windows.Forms.TextBox txtAddHero;
+        private System.Windows.Forms.TextBox txtRemoveHero;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAddHero;
+        private System.Windows.Forms.Button btnRemoveHero;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Part 7 Lists Assignment/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_7_Lists_Assignment
+{
+    public partial class frmLists : Form
+    {
+
+        List<int> numbers = new List<int>();
+        List<string> heroes = new List<string>();
+        Random generator = new Random();
+        int totalremove = 0;
+
+        public frmLists()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLists_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+                numbers.Add(generator.Next(100));
+            lstNumbers.DataSource = numbers;
+            heroes.Clear();
+            heroes.Add("Flash");
+            heroes.Add("Mr. Aldworth");
+            heroes.Add("Spiderman");
+            lstHeroes.DataSource = heroes;
+        }
+
+        private void btnSortNumbers_Click(object sender, EventArgs e)
+        {
+            numbers.Sort();
+            UpdateNumbers();
+            lblStatus.Text = ("Status: Numbers Sorted!");
+        }
+
+        private void btnSortHeroes_Click(object sender, EventArgs e)
+        {
+            heroes.Sort();
+            UpdateHeroes();
+            lblStatus.Text = ("Status: Heroes Sorted!");
+        }
+
+        private void btnNewNumbers_Click(object sender, EventArgs e)
+        {
+            numbers.Clear();
+            for (int i = 0; i < 20; i++)
+                numbers.Add(generator.Next(100));
+            UpdateNumbers();
+            lblStatus.Text = ("Status: New Numbers List!");
+        }
+        
+        private void btnNewHeroes_Click(object sender, EventArgs e)
+        {
+            heroes.Clear();
+            heroes.Add("Flash");
+            heroes.Add("Mr. Aldworth");
+            heroes.Add("Spiderman");
+            UpdateHeroes();
+            lblStatus.Text = ("Status: New Heroes List!");
+        }       
+        private void btnRemoveNumber_Click(object sender, EventArgs e)
+        {
+            if (lstNumbers.SelectedIndex < 0)
+                lblStatus.Text = "Status: No Item Selected!";
+            else
+            {
+                numbers.RemoveAt((Int32)lstNumbers.SelectedIndex);
+                UpdateNumbers();
+                lblStatus.Text = ("Status: Number Removed!");
+            }
+
+        }
+        private void btnRemoveAllNumbers_Click(object sender, EventArgs e)
+        {
+            if (lstNumbers.SelectedIndex < 0)
+                lblStatus.Text = "Status: No Item Selected!";
+            else
+                while (numbers.Remove((Int32)lstNumbers.SelectedItem))
+                {
+                    totalremove = totalremove + 1;
+                    lblStatus.Text = ($"Status: Removed a Total of {totalremove}!");
+                }
+            totalremove = 0;
+
+            UpdateNumbers();
+        }
+
+        private void btnRemoveHero_Click(object sender, EventArgs e)
+        {
+            if (heroes.Remove(txtRemoveHero.Text))
+            {
+                UpdateHeroes();
+                lblStatus.Text = ("Status: Heroes Removed!");
+            }
+            else
+                lblStatus.Text = ("Status: Hero is not in the List!");
+        }
+        
+            
+        private void UpdateHeroes()
+        {
+            lstHeroes.DataSource = null;
+            lstHeroes.DataSource = heroes;
+        }
+
+        private void UpdateNumbers()
+        {
+            lstNumbers.DataSource = null;
+            lstNumbers.DataSource = numbers;
+        }
+        private void btnAddHero_Click(object sender, EventArgs e)
+        {
+            if (heroes.Contains(txtAddHero.Text))
+                lblStatus.Text = ("Status: Hero Already in the List!");
+            else
+                heroes.Add(txtAddHero.Text);
+            lblStatus.Text = ("Status: Heroes added!");
+            
+            UpdateHeroes();
+        }
+
+    }
+}
